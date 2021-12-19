b89aa7d569df313fb7ae677de3531d59ab9a2bbd
JimenaKatuMartinez <93katu@gmail.com>
2021-12-19 16:58:01 -0300

    Revert "Merge branch 'agregar-lista' into login"
    This reverts commit dc856ec14c118113ae209c1d6a4467dba7db8ce5, reversing
    changes made to 9f87f9f21999baa4ed2c576121ed77f3852ff278.

--- src/models/Pelicula.js
+++ src/models/Pelicula.js
@@ -1,20 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const peliculaSchema = mongoose.Schema({
-  codigo: { type: Number },
-  nombre: { type: String },
-  director: { type: String },
-  protagonistas: { type: String },
-  duracion: { type: String },
-  trailer: { type: String },
-  imagen: { type: String },
-  fecha_de_Estreno: { type: Number },
-  sinopsis: { type: String },
-  genero: { type: String },
-  publicado: { type: Boolean },
-  esPelicula: { type: Boolean, default: true },
-  destacada: { type: Boolean, default: false },
-  vista: { type: Boolean, default: false },
-});
-const Pelicula = mongoose.model("Pelicula", peliculaSchema);
-module.exports = Pelicula;
+    codigo: Number,
+    nombre: String,
+    director: String,
+    protagonistas: String,
+    duracion: String,
+    trailer: String,
+    fecha_de_Estreno: String,
+    descripcion: String,
+    categoria: String,
+    publicado: Boolean,
+    destacado: Boolean,
+    vista: {type:Boolean, default:false}
+})
+const Pelicula = mongoose.model('Pelicula',peliculaSchema)
+module.exports = Pelicula
